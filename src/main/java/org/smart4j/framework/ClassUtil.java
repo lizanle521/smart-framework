2b8f7ec1b00a7c87392f8f16a7346ba3fc92ee6c
lizanle <491823689@qq.com>
2017-03-11 01:01:05 +0800

    包下的类加载

--- src/main/java/org/smart4j/framework/ClassUtil.java
+++ src/main/java/org/smart4j/framework/ClassUtil.java
@@ -1,13 +1,19 @@
 package org.smart4j.framework;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
+import java.net.JarURLConnection;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * 类操作工具
@@ -57,7 +63,24 @@ public final class ClassUtil {
                 if(url != null){
                     String protocol = url.getProtocol();
                     if("file".equals(protocol)){
-
+                        String packagePath = url.getPath().replaceAll("%20", "");
+                        addClass(classSet,packagePath,packageName);
+                    }else if("jar".equals(protocol)){
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if(jarURLConnection != null){
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if(jarFile != null){
+                                Enumeration<JarEntry> entries = jarFile.entries();
+                                while (entries.hasMoreElements()){
+                                    JarEntry jarEntry = entries.nextElement();
+                                    String name = jarEntry.getName();
+                                    if(name.endsWith(".class")){
+                                        String clasName = name.substring(0, name.lastIndexOf("."));
+                                        doAddClass(classSet,clasName);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -66,6 +89,45 @@ public final class ClassUtil {
             throw  new RuntimeException(e);
         }
         return  classSet;
+    }
+
+    /**
+     *
+     * @param classSet
+     * @param packagePath
+     * @param packageName
+     */
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
+            public boolean accept(File file) {
+                return file.isFile() && file.getName().endsWith(".class") || file.isDirectory();
+            }
+        });
+        for(File file : files){
+            String name = file.getName();
+            if(file.isFile()){
+                String className = name.substring(0,name.lastIndexOf("."));
+                if(StringUtils.isNotEmpty(packagePath)){
+                    className = packagePath+"."+className;
+                }
+                doAddClass(classSet,className);
+            }else{
+                String subpackagePath = name;
+                if(StringUtils.isNotEmpty(packagePath)){
+                    subpackagePath = packagePath + "/" + subpackagePath;
+                }
+                String subpackagename = name;
+                if(StringUtils.isNotEmpty(packageName)){
+                    subpackagename = packageName + "." + subpackagename;
+                }
+                addClass(classSet,subpackagePath,packageName);
+            }
+        }
+    }
+
+    private static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> cls = loadClass(className, false);
+        classSet.add(cls);
     }
 
 
