d66b56f23d3181ee0493eb1e010c7a9d7f6badda
lizanle <491823689@qq.com>
2017-04-16 00:45:15 +0800

    理清一下 aop 的逻辑

--- src/main/java/org/smart4j/framework/helper/AopHelper.java
+++ src/main/java/org/smart4j/framework/helper/AopHelper.java
@@ -13,7 +13,8 @@ import java.util.*;
 public final  class AopHelper {
 
     /**
-     * 获取所有带Acpect注解的类
+     * 获取所有带Acpect注解里边声明的类
+     * 譬如 @Aspect(Controller.class),那么获取的是有Controller注解的类
      * @param aspect
      * @return
      * @throws Exception
@@ -29,6 +30,8 @@ public final  class AopHelper {
 
     /**
      * 获取代理类和目标类之间的关系
+     * 这里代理类不确定，但是代理都是AspectProxy的子类，并且有Aspect注解
+     * 目标类是 每个类上边有AspectProxy子类的注解的类
      * @return
      * @throws Exception
      */
@@ -45,7 +48,30 @@ public final  class AopHelper {
         return classHashMap;
     }
 
+    /**
+     * 根据代理类与目标类之间的关系，可以得出代理类与目标对象之间的映射关系
+     * @param proxyMap
+     * @return
+     * @throws Exception
+     */
     private static Map<Class<?>,List<Proxy>> createTargetMap(Map<Class<?>,Set<Class<?>>> proxyMap) throws Exception{
-        return null;
+        HashMap<Class<?>,List<Proxy>> classHashMap = new HashMap<Class<?>,List<Proxy>>();
+        Set<Map.Entry<Class<?>, Set<Class<?>>>> entries = proxyMap.entrySet();
+        for (Map.Entry<Class<?>, Set<Class<?>>> entry : entries) {
+            Class<?> key = entry.getKey();
+            Set<Class<?>> value = entry.getValue();
+            for (Class<?> cls : value) {
+                Proxy proxy = (Proxy)key.newInstance();
+                if(classHashMap.containsKey(cls)){
+                    classHashMap.get(cls).add(proxy);
+                }else{
+                    List<Proxy> proxyList = new ArrayList<Proxy>();
+                    proxyList.add(proxy);
+                    classHashMap.put(cls,proxyList);
+                }
+            }
+        }
+        return classHashMap;
     }
+
 }
